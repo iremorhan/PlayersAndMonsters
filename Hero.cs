0e501bbd25d2654cbc29258ace4493e350649ce9
User <User@DESKTOP-7DEACCV>
2023-01-30 09:48:20 +0200

    Add project files.

---
+++ Hero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        string username;
+        int level;
+
+        public string Username { get => username; set => username = value; }
+        public int Level { get => level; set => level = value; }
+
+        public Hero(string username, int level)
+        {
+            Username = this.username;
+            Level = this.level;
+        }
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level} ";
+        }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+           string type =Console.ReadLine();
+           string username =Console.ReadLine();
+            int level =int.Parse( Console.ReadLine());
+            if (type=="Hero")
+            {
+                Hero hero = new Hero(username, level);
+                Console.WriteLine(hero);
+            }
+            else if (type=="Elf")
+            {
+                Elf hero = new Elf(username, level);
+                Console.WriteLine(hero);
+            }
+            else if (type == "MuseElf")
+            {
+                MuseElf hero = new MuseElf(username, level);
+                Console.WriteLine(hero);
+            }
+            else if (type == "Wizard")
+            {
+                Wizard hero = new Wizard(username, level);
+                Console.WriteLine(hero);
+            }
+            else if (type == "DarkWizard")
+            {
+                DarkWizard hero = new DarkWizard(username, level);
+                Console.WriteLine(hero);
+            }
+            else if (type == "SoulMaster")
+            {
+                SoulMaster hero = new SoulMaster(username, level);
+                Console.WriteLine(hero);
+            }
+            else if (type == "Knight")
+            {
+                Knight hero = new Knight(username, level);
+                Console.WriteLine(hero);
+            }
+            else if (type == "DarkKnight")
+            {
+                DarkKnight hero = new DarkKnight(username, level);
+                Console.WriteLine(hero);
+            }
+            else if (type == "BladeKnight")
+            {
+                BladeKnight hero = new BladeKnight(username, level);
+                Console.WriteLine(hero);
+            }
+
+        }
+    }
+}
